83ac28745fc5c7b358a905a7cf2a853e418edf65
saigerutherford <saruther@med.umich.edu>
2021-12-08 18:56:10 +0100

    add files

---
+++ 3d-scatter-plots.py
@@ -0,0 +1,35 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+df = px.data.iris()
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id="scatter-plot"),
+    html.P("Petal Width:"),
+    dcc.RangeSlider(
+        id='range-slider',
+        min=0, max=2.5, step=0.1,
+        marks={0: '0', 2.5: '2.5'},
+        value=[0.5, 2]
+    ),
+])
+
+@app.callback(
+    Output("scatter-plot", "figure"), 
+    [Input("range-slider", "value")])
+def update_bar_chart(slider_range):
+    low, high = slider_range
+    mask = (df.petal_width > low) & (df.petal_width < high)
+    
+    fig = px.scatter_3d(df[mask], 
+        x='sepal_length', y='sepal_width', z='petal_width',
+        color="species", hover_data=['petal_width'])
+    return fig
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
